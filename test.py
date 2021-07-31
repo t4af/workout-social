bc93f4d2ee9fe7033ff5b39f60f0621e9eda4c44
Johnny Fok <johnny.y.fok@gmail.com>
2021-07-30 19:53:19 -0700

    Initial Implementation of Weps

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
 print("Hello World!")
+
+numberOfExercises = input("How many exercises are you gonna do?\n")
+numberOfExercises = int(numberOfExercises)
+for x in range(0,numberOfExercises):
+    exercise = input("What exercise?\n")
+
+    numOfReps = input("How many reps did you do?\n")
+    answer = int(numOfReps)
+    answer = answer * 2
+    print("You did " + str(exercise) + (" and got ") + str(answer) + " points")
